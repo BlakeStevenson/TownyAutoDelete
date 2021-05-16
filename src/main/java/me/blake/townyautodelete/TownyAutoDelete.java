bcc9813cb1e92b88c926245f4785d98c3b76e5aa
Blake Stevenson <blake@artex.dev>
2021-05-16 13:39:26 -0500

    Add exemptions to config.yml, bump to 1.1

--- src/main/java/me/blake/townyautodelete/TownyAutoDelete.java
+++ src/main/java/me/blake/townyautodelete/TownyAutoDelete.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 public class TownyAutoDelete extends JavaPlugin {
     private final FileConfiguration config = this.getConfig();
+
     public void onEnable() {
         setupConfig();
 
@@ -26,30 +27,33 @@ public class TownyAutoDelete extends JavaPlugin {
         this.saveDefaultConfig();
         config.addDefault("inactiveLength", 336);
         config.addDefault("interval", 60);
+        config.set("version", "1.1-SNAPSHOT");
     }
 
     public void deleteTowns() {
         Collection<Town> towns = TownyUniverse.getInstance().getTowns();
+        for (Town t : towns) {
+            if (!config.getStringList("exemptions").contains(t.getName())) {
 
-        for(Town t : towns) {
-            List<Resident> residents = t.getResidents();
-            int numInactive = 0;
+                List<Resident> residents = t.getResidents();
+                int numInactive = 0;
 
-            for(Resident r : residents) {
-                if(r.getPlayer() == null) {
-                    Date lastJoinDate = new Date(this.getServer().getOfflinePlayer(r.getUUID()).getLastPlayed());
-                    Date now = new Date();
-                    long difference = now.getTime() - lastJoinDate.getTime();
-                    long differenceInHours = (difference / (1000 * 60 * 60)) % 24;
-                    if(differenceInHours >= config.getInt("inactiveLength")) {
-                        numInactive++;
+                for (Resident r : residents) {
+                    if (r.getPlayer() == null) {
+                        Date lastJoinDate = new Date(this.getServer().getOfflinePlayer(r.getUUID()).getLastPlayed());
+                        Date now = new Date();
+                        long difference = now.getTime() - lastJoinDate.getTime();
+                        long differenceInHours = (difference / (1000 * 60 * 60)) % 24;
+                        if (differenceInHours >= config.getInt("inactiveLength")) {
+                            numInactive++;
+                        }
                     }
                 }
-            }
 
-            if(numInactive == residents.size()) {
-                getServer().broadcastMessage(ChatColor.RED + t.getFormattedName() + " has fallen!");
-                TownyUniverse.getInstance().getDataSource().removeTown(t);
+                if (numInactive == residents.size()) {
+                    getServer().broadcastMessage(ChatColor.RED + t.getFormattedName() + " has fallen!");
+                    TownyUniverse.getInstance().getDataSource().removeTown(t);
+                }
             }
         }
     }
